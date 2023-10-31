e00012748282229d510cd711667796d40e745478
hikingyoung <37426376+hikingyoung@users.noreply.github.com>
2023-10-31 17:45:33 +0800

    添加了击打音效，其它零星修改

--- GDScripts/PingPongPlayer.gd
+++ GDScripts/PingPongPlayer.gd
@@ -3,20 +3,33 @@ extends Area2D
 @export var bPlayer1 = true
 @export var speed = 1
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
+	self.connect("TedDefineSignal", Callable(self, "SelfDefineFuncForSignal"))
+	emit_signal("TedDefineSignal", "我被触发了！")
 	pass # Replace with function body.
 
+signal TedDefineSignal
+
+func SelfDefineFuncForSignal(b):
+	print(b)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	for i in get_overlapping_areas():
-		if i.is_in_group("Ball"):
-			i.vec.x = NewX
-
-	
 	if  bPlayer1 :
 		self.position.y += (Input.get_action_strength("MoveDown")  - Input.get_action_strength("MoveUp"))*speed
 	else :
 		self.position.y += (Input.get_action_strength("SpeedDown") - Input.get_action_strength("SpeedUp"))*speed 
 	
 	pass
+
+
+
+
+
+func _on_area_entered(area):
+	if area.is_in_group("Ball"):
+		area.vec.x = NewX
+		#get_node("AudioStreamPlayer2D").play()
+		$AudioStreamPlayer2D.play()
+	pass # Replace with function body.
