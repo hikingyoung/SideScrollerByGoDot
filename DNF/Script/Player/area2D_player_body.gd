d54fd9f85e875ff1aa2d8c74e5d2000d4fc56034
YourOwnPC2017 <hikingyoung@icloud.com>
2023-11-15 00:33:00 +0800

    开始制作敌人——一个鬼魂，已经新建鬼魂场景，并制作好了相关动画，下一步是写状态机

--- DNF/Script/Player/area2D_player_body.gd
+++ DNF/Script/Player/area2D_player_body.gd
@@ -1,4 +1,5 @@
 extends Area2D
+#将由对方的伤害框的脚本来触发信号，本信号已绑定到当前场景的根节点中
 signal be_hit(damage:int)
 
 # Called when the node enters the scene tree for the first time.
--- DNF/Script/SwordManRootNode_Foot.gd
+++ DNF/Script/SwordManRootNode_Foot.gd
@@ -2,9 +2,7 @@ extends CharacterBody2D
 class_name Player
 @onready var state_machine:StateMachine = self.get_node("StateMachine")
 
-var ghost_scene =  load("res://PingPong/Scenes/ghost.tscn")
-@onready var sprite_2d = $Sprite2D
-@onready var timer = $Timer
+
 
 func _physics_process(delta):
 	
@@ -20,23 +18,5 @@ func _ready():
 func _on_area_2d_player_body_be_hit(damage):
 	print("糟糕！我被击中了！本次扣除血量 %s" %damage)
 	state_machine.emit_signal("transitioned", "Hurt")
-	pass # Replace with function body.
-
-func add_ghost():
-	var ghost = ghost_scene.instantiate()
-	print(self.position)
-	ghost.set_scale(Vector2(0.3,0.3))
-	ghost.set_new_position(self.position)
-	ghost.position.y += -120
-	print("ghost 的postition是%s" %ghost.position)
-	#sprite_2d.add_child(new_ghost)
-	get_tree().current_scene.add_child(ghost)
-	return
-
-
-func _on_timer_timeout():
-	print("时间到了")
-	add_ghost()
-	
 	pass # Replace with function body.
 
